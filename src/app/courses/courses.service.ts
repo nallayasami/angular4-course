f3f9a6741d75e6230c323fdbae71da2e8a63d974
Nallayasami Kathiresan <nka@sigos.com>
2017-11-23 16:13:01 +0100

    Angualr course - Section 4:Lecture 36

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -1,8 +1,7 @@
+import { Injectable } from '@angular/core';
 
-
+@Injectable()
 export class CoursesServcie{
-
-
     getCourses():Array<String>{
         return ["Course 1", "Course 2", "Course 3"];   
     }
