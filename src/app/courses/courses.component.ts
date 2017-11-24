7b15cfd8b1668f9b3ac863907aefbad336725d3c
Nallayasami Kathiresan <nka@sigos.com>
2017-11-24 18:14:29 +0100

    Angualr course - Section 4:Lecture 43

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -8,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  title:string = "Course List";
+  title:string="Course List";
+  buttonStr:String="Property binding";
+  primaryBtn:boolean= true;
+  eventStatus:String="";
+  counter:number=0;
+
   courses:Array<String>;
 
   constructor(service: CoursesServcie) {
     this.courses = service.getCourses();
    }
+
+   btnClicked(){
+     this.eventStatus = "Button clicked : ".concat((this.counter++).toString());
+   }
+
+   keyPressed($event){
+    this.eventStatus="Key Pressed : ".concat($event.keyCode);
+   }
+   
 
   ngOnInit() {
   }
