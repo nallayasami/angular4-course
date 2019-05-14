6758eafeddb1dd31cd72aa15cfaa1128b3c82f0c
Nallayasami <nallayasamik@live.com>
2019-05-14 22:20:15 +0200

    fragment Example

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { UserService } from './../common/service/user.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private service: UserService) { }
+  constructor(private service: UserService, private route: ActivatedRoute) { }
 
   users: Array<any>;
 
@@ -20,6 +21,12 @@ export class UserComponent implements OnInit {
     this.service.getAll().subscribe((response: Array<any>) => {
       response.push({ id: 123, name: 'invalid URL', email: 'invalid@test.com' });
       this.users = response;
+    });
+
+    this.route.fragment.subscribe((fragment: string) => {
+      if (fragment && document.getElementById(fragment) != null) {
+        document.getElementById(fragment).scrollIntoView({ behavior: "smooth" });
+      }
     });
   }
 
