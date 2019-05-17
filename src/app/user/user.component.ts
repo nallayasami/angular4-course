2a5501badf663c8a738ecdec02b5565fdad6523b
Nallayasami <nallayasamik@live.com>
2019-05-17 22:55:54 +0200

    asnyc loading issue fixed

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,21 @@
 import { UserService } from './../common/service/user.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, OnChanges, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
 
   constructor(private service: UserService, private route: ActivatedRoute) { }
 
   users: Array<any>;
+  aboutSubscription: Subscription;
 
+  @ViewChild('about') about: ElementRef;
   addPost(input: HTMLElement) {
 
   }
@@ -22,12 +25,20 @@ export class UserComponent implements OnInit {
       response.push({ id: 123, name: 'invalid URL', email: 'invalid@test.com' });
       this.users = response;
     });
-
-    this.route.fragment.subscribe((fragment: string) => {
-      if (fragment && document.getElementById(fragment) != null) {
-        document.getElementById(fragment).scrollIntoView({ behavior: "smooth" });
-      }
+    this.aboutSubscription = this.route.fragment.subscribe((fragment: string) => {
+      this.focusToFragment();
     });
   }
-
+  focusToFragment(): void {
+    const fragment = this.route.snapshot.fragment;
+    console.log('fragment:', fragment);
+    if ("about" == fragment) {
+      this.about.nativeElement.scrollIntoView();
+    }
+  }
+  ngOnDestroy() {
+    if(this.aboutSubscription) {
+      this.aboutSubscription.unsubscribe();
+    }
+  }
 }
